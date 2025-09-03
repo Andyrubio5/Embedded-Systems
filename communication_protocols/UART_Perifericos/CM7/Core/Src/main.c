5c6237d5f041be0e741200da83254c71215bf142
Andyrubio5 <Andyquezada005@gmail.com>
2025-09-02 19:03:33 -0600

    Report

--- communication_protocols/UART_Perifericos/CM7/Core/Src/main.c
+++ communication_protocols/UART_Perifericos/CM7/Core/Src/main.c
@@ -1,113 +1,286 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-#include "stm32h7xx.h"
-#include <stdint.h>
+/* USER CODE END Includes */
 
-/* ====================== Parámetros ====================== */
-#define UART_BAUDRATE     9600u          // Baud rate deseado
-#define USART3_CLK_HZ     64000000u     // Se esta usando el reloj HSI que es de 64 MHz
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-/* ====================== Utilidad BRR ==================== */
-// Parte del codigo encargada de que mi reloj tenga el valor correcto para el registro BRR
-// BRR = Tiempo exacto de cada bit
-static inline uint32_t brr_from(uint32_t fclk, uint32_t baud) {
-    return (fclk + (baud/2u)) / baud;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* DUAL_CORE_BOOT_SYNC_SEQUENCE: Define for dual core boot synchronization    */
+/*                             demonstration code based on hardware semaphore */
+/* This define is present in both CM7/CM4 projects                            */
+/* To comment when developping/debugging on a single core                     */
+#define DUAL_CORE_BOOT_SYNC_SEQUENCE
+
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+#ifndef HSEM_ID_0
+#define HSEM_ID_0 (0U) /* HW semaphore 0*/
+#endif
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MPU_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+/* USER CODE BEGIN Boot_Mode_Sequence_0 */
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+  int32_t timeout;
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+/* USER CODE END Boot_Mode_Sequence_0 */
+
+  /* MPU Configuration--------------------------------------------------------*/
+  MPU_Config();
+
+/* USER CODE BEGIN Boot_Mode_Sequence_1 */
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+  /* Wait until CPU2 boots and enters in stop mode or timeout*/
+  timeout = 0xFFFF;
+  while((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) != RESET) && (timeout-- > 0));
+  if ( timeout < 0 )
+  {
+  Error_Handler();
+  }
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+/* USER CODE END Boot_Mode_Sequence_1 */
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+/* USER CODE BEGIN Boot_Mode_Sequence_2 */
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+/* When system initialization is finished, Cortex-M7 will release Cortex-M4 by means of
+HSEM notification */
+/*HW semaphore Clock enable*/
+__HAL_RCC_HSEM_CLK_ENABLE();
+/*Take HSEM */
+HAL_HSEM_FastTake(HSEM_ID_0);
+/*Release HSEM in order to notify the CPU2(CM4)*/
+HAL_HSEM_Release(HSEM_ID_0,0);
+/* wait until CPU2 wakes up from stop mode */
+timeout = 0xFFFF;
+while((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) == RESET) && (timeout-- > 0));
+if ( timeout < 0 )
+{
+Error_Handler();
+}
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+/* USER CODE END Boot_Mode_Sequence_2 */
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
-/* ====================== DWT para delay ================== */
-/// Se utiliza el DWT como un contador de ticks,
-// lo que hace esta parte del codigo es que calcula por ticks, cuanto dura un segundo y asi volver a mandar el caracter
-static void DWT_Init(void) {
-    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;  // habilita el bloque de trazas (necesario para DWT)
-    DWT->LAR = 0xC5ACCE55;                           // desbloqueo (algunos H7 lo requieren)
-    DWT->CYCCNT = 0;                                 // limpia el contador de ciclos
-    DWT->CTRL  |= DWT_CTRL_CYCCNTENA_Msk;            // enciende el contador de ciclos
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY);
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-static void delay_ms(uint32_t ms) {
-    uint32_t start = DWT->CYCCNT;
-    uint64_t ticks = ((uint64_t)SystemCoreClock * ms) / 1000u;
-    while ((uint32_t)(DWT->CYCCNT - start) < ticks) { /* espera ocupada */ }
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
-/* ================== GPIO: PB10/PB11 como AF7 = USART3 ================== */
-// Esta parte del codigo se encarga de poner los GPIO en el camino del USART3
-static void GPIO_Init_USART3_PB10_PB11(void) {
-    // 1) Habilita el reloj del puerto B
-    RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN;
+/* USER CODE BEGIN 4 */
 
-    // 2) Modo Alternate Function en PB10 y PB11 (MODER: 10b)
-    GPIOB->MODER &= ~((3u<<(10*2)) | (3u<<(11*2)));
-    GPIOB->MODER |=  ((2u<<(10*2)) | (2u<<(11*2)));
+/* USER CODE END 4 */
 
-    // El modo AF  lo que hace es conectarlo a un periferico interno
+ /* MPU Configuration */
 
-    // 3) Selecciona AF7 (USART3) en AFR[1] (pines 8..15)
-    GPIOB->AFR[1] &= ~((0xFu<<((10-8)*4)) | (0xFu<<((11-8)*4)));
-    GPIOB->AFR[1] |=  ((7u  <<((10-8)*4)) | (7u  <<((11-8)*4)));
+void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
 
-    // 4) Push-pull y velocidad muy alta (mejores flancos en TX)
-    GPIOB->OTYPER  &= ~((1u<<10) | (1u<<11));
-    GPIOB->OSPEEDR |=  (3u<<(10*2)) | (3u<<(11*2));   // 11b = very high
+  /* Disables the MPU */
+  HAL_MPU_Disable();
 
-    // 5) Pull-up en RX (PB11) para mantener línea en 'idle' alto
-    GPIOB->PUPDR   &= ~(3u<<(11*2));
-    GPIOB->PUPDR   |=  (1u<<(11*2));                  // 01b = pull-up
+  /** Initializes and configures the Region and the memory to be protected
+  */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0x0;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+  MPU_InitStruct.SubRegionDisable = 0x87;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+  /* Enables the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
 }
 
-/* ================== USART3: 9600 8N1 por registros ================== */
-/* CR1/CR2/CR3 en modo básico: 8 bits, sin paridad, 1 stop, oversampling x16. */
-static void USART3_Init_9600_8N1(void) {
-    // 1) Habilita el reloj del USART3
-    RCC->APB1LENR |= RCC_APB1LENR_USART3EN;
-
-    // 2) Reset corto para arrancar “limpio”
-    RCC->APB1LRSTR |=  RCC_APB1LRSTR_USART3RST;
-    RCC->APB1LRSTR &= ~RCC_APB1LRSTR_USART3RST;
-
-    // 3) Asegúrate de que esté deshabilitado antes de configurar
-    USART3->CR1 &= ~USART_CR1_UE;
-
-    // 4) Config básica: 8N1, sin paridad, sin flow/DMA
-    USART3->CR1 = 0x00000000u;   // M1/M0=0 (8 bits), PCE=0, OVER8=0 (x16)
-    USART3->CR2 = 0x00000000u;   // STOP=00 (1 stop)
-    USART3->CR3 = 0x00000000u;   // sin RTS/CTS, sin DMA
-
-    // 5) Baud rate
-    USART3->BRR = brr_from(USART3_CLK_HZ, UART_BAUDRATE);
-
-    // 6) Habilita solo el transmisor (TE) y luego el periférico (UE)
-    USART3->CR1 |= USART_CR1_TE;
-    USART3->CR1 |= USART_CR1_UE;
-
-    // 7) Espera confirmación de transmisión habilitada
-    while ((USART3->ISR & USART_ISR_TEACK) == 0u) { /* wait */ }
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
-
-/* ================== Envío de un byte (polling) ================== */
-/* Espera a que el TDR/FIFO tenga espacio y escribe el byte en TDR. */
-static void USART3_SendByte(uint8_t b) {
-    while ((USART3->ISR & USART_ISR_TXE_TXFNF) == 0u) { /* wait */ }
-    USART3->TDR = b;
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
-
-/* ============================== main ============================== */
-int main(void) {
-    // 0) Asegura que SystemCoreClock esté correcto (para DWT)
-    SystemCoreClockUpdate();
-
-    // 1) Inicializa el contador de ciclos (para delay_ms)
-    DWT_Init();
-
-    // 2) Configura PB10/PB11 como AF7 (USART3)
-    GPIO_Init_USART3_PB10_PB11();
-
-    // 3) Inicializa USART3 a 9600-8N1
-    USART3_Init_9600_8N1();
-
-    // 4) Bucle: enviar 'U' (0x55) una vez por segundo
-    for (;;) {
-        USART3_SendByte('H');   // patrón 01010101 (LSB primero), ideal para medir
-        delay_ms(1000);         // espera ~1 s entre envíos
-    }
-}
+#endif /* USE_FULL_ASSERT */
